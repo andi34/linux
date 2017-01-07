5162c625d312740e07e3cd3304e3f674cc1d58ed
Simon Shields <keepcalm444@gmail.com>
2017-01-07 19:59:14 +1100

    Revert "USB: gadget: s3c-hsotg: fix "protocol stall" handling"
    This reverts commit d479074f7f77b6fc78efc0cbf897e54c9d2223be.

--- drivers/usb/gadget/s3c-hsotg.c
+++ drivers/usb/gadget/s3c-hsotg.c
@@ -1151,8 +1151,6 @@ static int s3c_hsotg_process_req_feature(struct s3c_hsotg *hsotg,
 	return 1;
 }
 
-static void s3c_hsotg_enqueue_setup(struct s3c_hsotg *hsotg);
-
 /**
  * s3c_hsotg_process_control - process a control request
  * @hsotg: The device state
@@ -1252,11 +1250,10 @@ static void s3c_hsotg_process_control(struct s3c_hsotg *hsotg,
 		 * don't believe we need to anything more to get the EP
 		 * to reply with a STALL packet
 		 */
-
-		 /* complete won't by called, so we enqueue setup request here */
-		 s3c_hsotg_enqueue_setup(hsotg);
 	}
 }
+
+static void s3c_hsotg_enqueue_setup(struct s3c_hsotg *hsotg);
 
 /**
  * s3c_hsotg_complete_setup - completion of a setup transfer
