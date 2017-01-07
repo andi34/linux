145085a3b3c400f8f64491855d9165e0186760b3
Simon Shields <keepcalm444@gmail.com>
2017-01-07 20:00:26 +1100

    Revert "usb: gadget: rndis: init & exit rndis at module load/unload"
    This reverts commit d356158d86683b1e187db4bc7c0567b8bcdf55ca.

--- drivers/usb/gadget/rndis.c
+++ drivers/usb/gadget/rndis.c
@@ -1174,7 +1174,7 @@ int rndis_init(void)
 
 	return 0;
 }
-module_init(rndis_init);
+EXPORT_SYMBOL(rndis_init);
 
 void rndis_exit(void)
 {
@@ -1188,6 +1188,6 @@ void rndis_exit(void)
 	}
 #endif
 }
-module_exit(rndis_exit);
+EXPORT_SYMBOL(rndis_exit);
 
 MODULE_LICENSE("GPL");
